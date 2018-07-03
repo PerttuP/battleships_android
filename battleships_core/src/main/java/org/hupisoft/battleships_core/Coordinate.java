cc8d6aa63d1b68814c31e4ed409eb1a2e3af8f98
Perttu Paarlahti <perttu.paarlahti@student.tut.fi>
2018-07-03 20:05:06 +0300

    Add Coordinate utility class to core library
    Coordinate indicates single location in game area.

---
+++ battleships_core/src/main/java/org/hupisoft/battleships_core/Coordinate.java
@@ -0,0 +1,61 @@
+package org.hupisoft.battleships_core;
+
+import java.util.ArrayList;
+
+public class Coordinate {
+
+    private int mX;
+    private int mY;
+
+    public Coordinate(int x, int y) {
+        mX = x;
+        mY = y;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other instanceof Coordinate) {
+            Coordinate otherCoord = (Coordinate)other;
+            return otherCoord.x() == mX && otherCoord.y() == mY;
+        }
+        return false;
+    }
+
+    /**
+     * Get x-coordinate.
+     * @return X-coordinate.
+     */
+    public int x() {
+        return mX;
+    }
+
+    /**
+     * Get y-coordinate.
+     * @return Y-coordinate.
+     */
+    public int y() {
+        return mY;
+    }
+
+    /**
+     * Get neigbor coordinates (horisontal, vertical and diagonal).
+     * @return List of neighbour cooordinates.
+     */
+    public ArrayList<Coordinate> neighbours() {
+        ArrayList<Coordinate> neighbours = new ArrayList<>();
+        for (int x = mX - 1; x <= mX+1; ++x) {
+            for (int y = mY - 1; y <= mY + 1; ++y) {
+                if (x == mX && y == mY) {
+                    continue;
+                } else {
+                    neighbours.add(new Coordinate(x, y));
+                }
+            }
+        }
+        return  neighbours;
+    }
+
+    public boolean isNeighbour(Coordinate other) {
+        return neighbours().contains(other);
+    }
+}
---
+++ battleships_core/src/test/java/org/hupisoft/battleships_core/CoordinateTest.java
@@ -0,0 +1,73 @@
+package org.hupisoft.battleships_core;
+
+import org.junit.Test;
+import java.util.ArrayList;
+import static org.junit.Assert.*;
+
+public class CoordinateTest {
+
+    @Test
+    public void coordinateIsConstructedWithCorrectXandY() {
+        for (int x = 0; x < 10; ++x) {
+            for (int y = 0; y < 10; ++y) {
+                Coordinate c = new Coordinate(x,y);
+                assertEquals(x, c.x());
+                assertEquals(y, c.y());
+            }
+        }
+    }
+
+    @Test
+    public void equalityTest() {
+        Coordinate c1 = new Coordinate(0,0);
+        Coordinate c2 = new Coordinate(0,1);
+        Coordinate c3 = new Coordinate(1,0);
+        Coordinate c4 = new Coordinate(0,0);
+        String notCoordinate = "asd";
+
+        assertEquals(c1, c4);
+        assertEquals(c1, c1);
+        assertEquals(c4, c1);
+        assertNotEquals(c1, c2);
+        assertNotEquals(c2,c1);
+        assertNotEquals(c1, c3);
+        assertNotEquals(c3, c4);
+
+        assertNotEquals(c1, notCoordinate);
+        assertNotEquals(notCoordinate, c1);
+        assertNotEquals(c1, null);
+        assertNotEquals(null, c1);
+    }
+
+    @Test
+    public void getNeighboursTest() {
+        Coordinate c = new Coordinate(0,0);
+
+        ArrayList<Coordinate> neighbours = c.neighbours();
+        assertEquals(8, neighbours.size());
+        assertTrue(neighbours.contains(new Coordinate(-1,-1)));
+        assertTrue(neighbours.contains(new Coordinate(-1,0)));
+        assertTrue(neighbours.contains(new Coordinate(-1,1)));
+        assertTrue(neighbours.contains(new Coordinate(0,-1)));
+        assertTrue(neighbours.contains(new Coordinate(0,1)));
+        assertTrue(neighbours.contains(new Coordinate(1,-1)));
+        assertTrue(neighbours.contains(new Coordinate(1,0)));
+        assertTrue(neighbours.contains(new Coordinate(1,1)));
+    }
+
+    @Test
+    public void isNeigbourTest() {
+        Coordinate c = new Coordinate(0,0);
+        ArrayList<Coordinate> neighbours = c.neighbours();
+        assertEquals(8, neighbours.size());
+
+        for (Coordinate inList : neighbours) {
+            assertTrue(c.isNeighbour(inList));
+            assertTrue(inList.isNeighbour(c));
+        }
+
+        Coordinate c2 = new Coordinate(5,5);
+        assertFalse(c.isNeighbour(c2));
+        assertFalse(c2.isNeighbour(c));
+    }
+}
