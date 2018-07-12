8c4facf11caaa270d00dad42b06a5635d40ed62b
Perttu Paarlahti <perttu.paarlahti@student.tut.fi>
2018-07-12 21:39:09 +0300

    Add stub implementation for battle view

--- app/src/main/java/org/hupisoft/battleships/MenuActivity.java
+++ app/src/main/java/org/hupisoft/battleships/MenuActivity.java
@@ -1,7 +1,10 @@
 package org.hupisoft.battleships;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        Button newVersusGameBtn = findViewById(R.id.newVersusGameBtn);
+        newVersusGameBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent toBattleViewIntent = new Intent(getApplicationContext(), BattleActivity.class);
+                startActivity(toBattleViewIntent);
+            }
+        });
     }
 }
