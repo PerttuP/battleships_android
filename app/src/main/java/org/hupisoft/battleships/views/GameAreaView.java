9e4556573c3a6ea871b954ddd03c762cd55d09be
Perttu Paarlahti <perttu.paarlahti@student.tut.fi>
2019-01-12 14:28:38 +0200

    Add coordinates to the sides of GameAreaView

--- app/src/main/java/org/hupisoft/battleships/views/GameAreaView.java
+++ app/src/main/java/org/hupisoft/battleships/views/GameAreaView.java
@@ -3,12 +3,20 @@ package org.hupisoft.battleships.views;
 import android.content.Context;
 import android.view.View;
 import android.widget.GridLayout;
+import android.widget.TextView;
 
 import org.hupisoft.battleships_core.Coordinate;
 import org.hupisoft.battleships_core.IGameArea;
 import org.hupisoft.battleships_core.ISquare;
 
 public class GameAreaView extends GridLayout {
+
+    private static final String[] ALPHABETS = {
+            "A", "B", "C", "D", "E", "F", "G",
+            "H", "I", "J", "K", "L", "M", "N",
+            "O", "P", "Q", "R", "S", "T", "U",
+            "V", "W", "X", "Y", "Z"
+    };
 
     private class SquareClickListener implements OnClickListener {
         private final Coordinate mLocation;
@@ -30,9 +38,42 @@ public class GameAreaView extends GridLayout {
 
     public void setArea(IGameArea area) {
         removeAllViews();
-        setColumnCount(area.width());
-        setRowCount(area.height());
+        setColumnCount(area.width() + 1);
+        setRowCount(area.height() + 1);
+        setCoordinates(area.width(), area.height());
+        setSquares(area);
+    }
 
+    private void setCoordinates(int width, int height) {
+        setHorizontalCoordinates(width);
+        setVerticalCoordinates(height);
+    }
+
+    private void setHorizontalCoordinates(int width) {
+        for (int x = 0; x < width; ++x) {
+            View view = createCoordinateView(ALPHABETS[x]);
+            GridLayout.Spec rowSpec = spec(0, 1);
+            GridLayout.Spec colSpec = spec(x+1, 1);
+            addView(view, new GridLayout.LayoutParams(rowSpec, colSpec));
+        }
+    }
+
+    private void setVerticalCoordinates(int height) {
+        for (int y = 0; y < height; ++y) {
+            View view = createCoordinateView(Integer.toString(y+1));
+            GridLayout.Spec rowSpec = spec(y+1, 1);
+            GridLayout.Spec colSpec = spec(0, 1);
+            addView(view, new GridLayout.LayoutParams(rowSpec, colSpec));
+        }
+    }
+
+    private View createCoordinateView(String text) {
+        TextView view = new TextView(getContext());
+        view.setText(text);
+        return view;
+    }
+
+    private void setSquares(IGameArea area) {
         for (int y = 0; y < area.height(); ++y) {
             for (int x = 0; x < area.width(); ++x) {
                 Coordinate c = new Coordinate(x,y);
@@ -41,8 +82,8 @@ public class GameAreaView extends GridLayout {
                 SquareClickListener sqrListener = new SquareClickListener(c);
                 square.setClickable(true);
                 square.setOnClickListener(sqrListener);
-                GridLayout.Spec rowSpec = spec(y, 1);
-                GridLayout.Spec colSpec = spec(x, 1);
+                GridLayout.Spec rowSpec = spec(y+1, 1);
+                GridLayout.Spec colSpec = spec(x+1, 1);
                 addView(square, new GridLayout.LayoutParams(rowSpec, colSpec));
             }
         }
